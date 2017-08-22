772f13f30ab478d5716b55295ab7d7cd87c1a311
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-08-22 13:35:57 +0200

    Calculator MSTest

---
+++ Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Calculator_Sum_6and9_Result15()
+        {
+            // Arrange
+            Calculator calc = new Calculator();
+
+            // Act
+            int result = calc.Sum(6, 9);
+
+            // Assert
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void Calculator_Sum_0and0_Result0()
+        {
+            // Arrange
+            Calculator calc = new Calculator();
+
+            // Act
+            int result = calc.Sum(0, 0);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Calculator_Sum_M5andM3_ResultM8()
+        {
+            // Arrange
+            Calculator calc = new Calculator();
+
+            // Act
+            int result = calc.Sum(-5, -3);
+
+            // Assert
+            Assert.AreEqual(-8, result);
+        }
+
+        [TestMethod]
+        public void Calculator_Sum_MAX_and_1_Result_OverflowException()
+        {
+            // Arrange
+            Calculator calc = new Calculator();
+
+            try
+            {
+                int result = calc.Sum(int.MaxValue, 1);
+                Assert.Fail("No Overflow Exception was thrown.");
+            }
+            catch (OverflowException)
+            { }
+            catch (Exception)
+            {
+                Assert.Fail("No Overflow Exception was thrown.");
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]  // Assert
+        public void Calculator_Sum_MIN_and_M1_Result_OverflowException()
+        {
+            // Arrange
+            Calculator calc = new Calculator();
+
+            // Act
+            int result = calc.Sum(int.MinValue, -1);
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        public int Sum(int zahl1, int zahl2)
+        { 
+            return checked(zahl1 + zahl2);
+        }
+    }
+}
